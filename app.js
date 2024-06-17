ccb384f76aa57beba2301861bdaf29376d3a701e
nfrisbee <nicolefrisbee1@gmail.com>
2024-06-17 13:33:21 -0600

    added IP addresses where I removed them previously

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-//Function to get user's IP address
+// Function to get user's IP address
 function getUserIP(callback) {
     $.getJSON('https://api.ipify.org?format=json', function(data) {
         callback(data.ip);
     });
 }
 
-
 // Defining the pingUser function
-function pingUser() { 
-    var settings = { 
+function pingUser(userIP) {
+    var ipAddresses = ['204.16.236.8', '204.16.238.8'];
+    var settings = {
         url: "http://localhost:3000/ping",
-        method: "POST", 
+        method: "POST",
         contentType: "application/json",
-        data: JSON.stringify({ ip: "user_ip_here" }),
+        data: JSON.stringify({ userIP: userIP, ipAddresses: ipAddresses }),
         success: function(response) {
             console.log(response);
-            //I will change the alert message after code is complete
             alert('Ping results received. Check console for details.');
         },
         error: function(error) {
@@ -24,12 +23,14 @@ function pingUser() {
         }
     };
 
-    // Sends the request and observes the response 
+    // Sends the request and observes the response
     $.ajax(settings);
-} 
+}
 
 $(document).ready(function() {
     $(".custom-button").click(function() {
-        pingUser();
+        getUserIP(function(userIP) {
+            pingUser(userIP);
+        });
     });
 });
